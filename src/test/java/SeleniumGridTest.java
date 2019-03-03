a45d394730c4cfaf379511a8d9f608d369169342
john <shinjia5@163.com>
2019-03-03 23:01:12 +0800

    fix docker-selenium test

--- src/test/java/SeleniumGridTest.java
+++ src/test/java/SeleniumGridTest.java
@@ -2,15 +2,12 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.impl.WebDriverContainer;
 import com.codeborne.selenide.impl.WebDriverThreadLocalContainer;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.impl.WebDriverContainer;
-import com.codeborne.selenide.impl.WebDriverThreadLocalContainer;
-import org.openqa.selenium.JavascriptExecutor;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 import static com.codeborne.selenide.Selectors.byText;
@@ -19,17 +16,28 @@ import static com.codeborne.selenide.Selenide.open;
 
 class SeleniumGridTest {
     @Test
-    void test(){
+    void test() {
+
+        ChromeOptions chrome_options = new ChromeOptions();
+//        chrome_options.addArguments("--headless");
+        chrome_options.addArguments("--no-sandbox");
+        chrome_options.addArguments("--disable-gpu");
+        chrome_options.addArguments("--disable-dev-shm-usage");
+
         WebDriverContainer webdriverContainer = new WebDriverThreadLocalContainer();
         Configuration.screenshots = false;
         webdriverContainer.clearBrowserCache();
-//
-        Configuration.browser="chrome";
+
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chrome_options);
+
+
+        Configuration.browser = "chrome";
+        Configuration.browserCapabilities = capabilities;
         Configuration.remote = "http://192.168.2.104:5001/wd/hub";
 
         String baseUrl = "http://192.168.2.104:8099/";
-
-        open(baseUrl);
+        com.codeborne.selenide.Selenide.open(baseUrl);
 
         $(byText("Welcome Gaofei!")).should(Condition.visible);
 
